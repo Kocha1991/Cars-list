6688c148494a5885dc9f959221050cbcc446258c
Kocherha Roman <f.your.d.2017@gmail.com>
2023-07-04 18:35:08 +0300

    add errors to form modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -31,6 +31,7 @@ export const Modal: React.FC<Props> = ({
       id: car?.id ?? new Date().getTime(),
       price: car?.price ?? '0',
     },
+    reValidateMode: "onChange"
   });
 
   return (
@@ -50,83 +51,132 @@ export const Modal: React.FC<Props> = ({
         </header>
         <section className='modal-card-body'>
           <div className='modal-item'>
-            <input
-              {...register('car')}
-              placeholder='Company'
-              className='input'
-              type='text'
-              disabled={!isAddCar}
-              required={isAddCar}
-            />
+            <div className='input-wrapper'>
+              <input
+                {...register('car', {
+                  required: isAddCar && "Fill this field",
+                })}
+                placeholder='Company'
+                className='input'
+                type='text'
+                disabled={!isAddCar}
+              />
+               <p className='tittle form-error'>{errors.car?.message}</p>
+            </div>
             <label className='modal-item__text'>Company</label>
           </div>
 
           <div className='modal-item'>
-            <input
-              {...register('car_model')}
-              placeholder='Model'
-              className='input'
-              type='text'
-              disabled={!isAddCar}
-              required={isAddCar}
-            />
+            <div className='input-wrapper'>
+              <input
+                {...register('car_model', {
+                  required: isAddCar && "Fill this field",
+                })}
+                placeholder='Model'
+                className='input'
+                type='text'
+                disabled={!isAddCar}
+              />
+               <p className='tittle form-error'>{errors.car_model?.message}</p>
+            </div>
             <label className='modal-item__text'>Model</label>
           </div>
 
           <div className='modal-item'>
-            <input
-              {...register('car_vin')}
-              placeholder='VIN'
-              className='input'
-              disabled={!isAddCar}
-              required={isAddCar}
-            />
-            <label className='modal-item__text'>VIN</label>
-            <p>{errors.car_vin?.message}</p>
+            <div className='input-wrapper'>
+              <input
+                {...register('car_vin', {
+                    required: isAddCar && "Fill this field",
+                    maxLength: {
+                      value: 17,
+                      message: 'You entered more than 17 characters',
+                    },
+                    minLength: {
+                      value: 17,
+                      message: 'You entered less than 17 characters',
+                    }
+                  })}
+                  placeholder='VIN'
+                  className='input'
+                  disabled={!isAddCar}
+                />
+              <p className='tittle form-error'>{errors.car_vin?.message}</p>
+            </div>
+            <label className='modal-item__text'>VIN</label>    
           </div>
 
           <div className='modal-item'>
-            <input
-              {...register('car_model_year')}
-              placeholder='Year'
-              className='input'
-              type='number'
-              disabled={!isAddCar}
-              required={isAddCar}
-            />
+            <div className='input-wrapper'>
+              <input
+                {...register('car_model_year', {
+                  required: isAddCar && "Fill this field",
+                  min: {
+                    value: 1991,
+                    message: 'You can add a car from 1991 year'
+                  },
+                  max: {
+                    value: 2023,
+                    message: "We can't travel to the future...)"
+                  },
+                })}
+                placeholder='Year'
+                className='input'
+                type='number'
+                disabled={!isAddCar}
+              />
+              <p className='tittle form-error'>{errors.car_model_year?.message}</p>
+            </div>
+            
             <label className='modal-item__text'>Year</label>
           </div>
 
           <div className='modal-item'>
-            <input
-              {...register('car_color')}
-              placeholder='Color'
-              className='input'
-              type='text'
-              required={isAddCar}
-            />
+            <div className='input-wrapper'>
+              <input
+                {...register('car_color', {
+                  required: isAddCar && "Fill this field",
+                })}
+                placeholder='Color'
+                className='input'
+                type='text'
+              />
+              <p className='tittle form-error'>{errors.car_color?.message}</p>
+            </div>
             <label className='modal-item__text'>Color</label>
           </div>
 
           <div className='modal-item'>
-            <input
-              {...register('price')}
-              placeholder='Price'
-              className='input'
-              type='number'
-              required={isAddCar}
-            />
+            <div className='input-wrapper'>
+              <input
+                {...register('price', {
+                  required: isAddCar && "Fill this field",
+                  min: {
+                    value: 1000,
+                    message: 'The minimum price starts at $1000'
+                  }
+                })}
+                placeholder='Price'
+                className='input'
+                type='number'
+              />
+              <p className='tittle form-error'>{errors.price?.message}</p>
+            </div>
             <label className='modal-item__text'>Price</label>
           </div>
 
           <div className='modal-item'>
-            <input
-              {...register('availability')}
-              id='Availability'
-              type='checkbox'
-              className='checkbox'
-              required={isAddCar}
-            />
+            <div className='input-wrapper input-wrapper__checkbox'>
+              <input
+                {...register('availability', {
+                  required: isAddCar && "Fill this field",
+                })}
+                id='Availability'
+                type='checkbox'
+                className='checkbox'
+              />
+              <p className='tittle form-error'>{errors.availability?.message}</p>
+            </div>
+            
             <label htmlFor='Availability' className='checkbox'>
               Availability
             </label>
