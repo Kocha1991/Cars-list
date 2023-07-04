5b86509e50f1bed88c1abb23ec40b0b4fe1cdbc0
Kocherha Roman <f.your.d.2017@gmail.com>
2023-07-04 09:39:35 +0300

    work with host

--- src/App.tsx
+++ src/App.tsx
@@ -10,154 +10,158 @@ import { debounce } from 'lodash';
 import { IsLoader } from './components/Loader/IsLoader';
 import { ModalDelete } from './components/Modal/ModalDelete';
 import { Button } from './components/Button/Button';
-import { TestModal } from './components/Modal/TestModal';
+import { Modal } from './components/Modal/Modal';
 
 import './App.scss';
 
 export enum SelectOptions {
   Action = 'Action',
   Edit = 'Edit',
-  Delete = 'Delete'
+  Delete = 'Delete',
 }
 
 const App: React.FC = () => {
-  const customCars: Car[] = JSON.parse(localStorage.getItem("customCars") ?? "[]")
+  const customCars: Car[] = JSON.parse(
+    localStorage.getItem('customCars') ?? '[]'
+  );
   const [cars, setCars] = useState<Car[]>([]);
   const [search, setSearch] = useState<string>('');
+  const [isAddCar, setIsAddCar] = useState<boolean>(false);
   const [isCarModalOpen, setIsCarModalOpen] = useState<boolean>(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [error, setError] = useState<string>('');
 
-  const [selectedCar, setSelectedCar] = useState<Car | undefined>(undefined)
-
- 
+  const [selectedCar, setSelectedCar] = useState<Car | undefined>(undefined);
 
   useEffect(() => {
     if (customCars.length) {
-      return setCars(customCars)
+      return setCars(customCars);
     }
     getCarsAPI().then((data) => {
-     
-      if(typeof data === 'string') {
-        return setError(data)
+      if (typeof data === 'string') {
+        return setError(data);
       }
 
-      localStorage.setItem("customCars", JSON.stringify(data.cars))
-      setCars(data.cars)
-    }
-   
-    );
+      localStorage.setItem('customCars', JSON.stringify(data.cars));
+      setCars(data.cars);
+    });
   }, []);
 
   const columns = [
-      {
-        name: 'Company',
-        selector: (row: Car) => row.car,
-        sortable: true,
-      },
-      {
-        name: 'Model',
-        selector: (row: Car) => row.car_model,
-        sortable: true,
-      },
-      {
-        name: 'Vin',
-        selector: (row: Car) => row.car_vin,
-        sortable: true,
-      },
-      {
-        name: 'Color',
-        selector: (row: Car) => row.car_color,
-        sortable: true,
-      },
-      {
-        name: 'Yer',
-        selector: (row: Car) => row.car_model_year,
-        sortable: true,
-      },
-      {
-        name: 'Price',
-        selector: (row: Car) => row.price,
-        sortable: true,
-      },
-      {
-        name: 'Availability',
-        selector: (row: Car) => row.availability ? 'Yes' : 'No',
-        cell: (row: Car) =>  <span style={{ color: row.availability ? 'blue' : 'red' }}>
-        {row.availability ? 'Yes' : 'No'}
-      </span>
-      },
-      {
-        name: '',
-        cell: (row: Car) => <select
+    {
+      name: 'Company',
+      selector: (row: Car) => row.car,
+      sortable: true,
+    },
+    {
+      name: 'Model',
+      selector: (row: Car) => row.car_model,
+      sortable: true,
+    },
+    {
+      name: 'Vin',
+      selector: (row: Car) => row.car_vin,
+      sortable: true,
+    },
+    {
+      name: 'Color',
+      selector: (row: Car) => row.car_color,
+      sortable: true,
+    },
+    {
+      name: 'Yer',
+      selector: (row: Car) => row.car_model_year,
+      sortable: true,
+    },
+    {
+      name: 'Price',
+      selector: (row: Car) => row.price,
+      sortable: true,
+    },
+    {
+      name: 'Availability',
+      selector: (row: Car) => (row.availability ? 'Yes' : 'No'),
+      cell: (row: Car) => (
+        <span style={{ color: row.availability ? 'blue' : 'red' }}>
+          {row.availability ? 'Yes' : 'No'}
+        </span>
+      ),
+    },
+    {
+      name: '',
+      cell: (row: Car) => (
+        <select
           className='select'
           name='action'
           value={SelectOptions.Action}
           onChange={(e) => handleChangeSelectedOptions(e, row)}
-        > 
-          <option 
-            selected 
-            disabled 
-            value={SelectOptions.Action}
-          >
+        >
+          <option selected disabled value={SelectOptions.Action}>
             Action
           </option>
           <option value={SelectOptions.Edit}>Edite</option>
           <option value={SelectOptions.Delete}>Delete</option>
-        </select>,
-   
-      },
-    ];
+        </select>
+      ),
+    },
+  ];
 
   const handleChangeSelectedOptions = (
     e: React.ChangeEvent<HTMLSelectElement>,
     car: Car
   ) => {
-    const action = e.target.value as SelectOptions
+    const action = e.target.value as SelectOptions;
     setSelectedCar(car);
 
     if (action === SelectOptions.Delete) {
-     return setIsDeleteModalOpen(true)
+      return setIsDeleteModalOpen(true);
     }
 
-    return setIsCarModalOpen(true)
-  }
+    return setIsCarModalOpen(true);
+  };
 
   const onCloseModal = () => {
-    if(isCarModalOpen) {
-      setIsCarModalOpen(false)
+    if (isCarModalOpen) {
+      setIsCarModalOpen(false);
     }
-    if (isDeleteModalOpen){
-      setIsDeleteModalOpen(false)
+
+    if (isAddCar) {
+      setIsAddCar(false);
     }
-    setSelectedCar(undefined)
-  }
+
+    if (isDeleteModalOpen) {
+      setIsDeleteModalOpen(false);
+    }
+
+    setSelectedCar(undefined);
+  };
 
   const removeCar = () => {
     localStorage.setItem(
-      "customCars", JSON.stringify(
-        cars.filter(car => car.id !== selectedCar?.id
-    )))
-    onCloseModal()
-  }
+      'customCars',
+      JSON.stringify(cars.filter((car) => car.id !== selectedCar?.id))
+    );
+    onCloseModal();
+  };
 
-  const onAddCar = () => setIsCarModalOpen(true)
-  
+  const onAddCar = () => {
+    setIsCarModalOpen(true);
+    setIsAddCar(true);
+  };
+
   const onSaveNewCar = (newCar: Car) => {
-    localStorage.setItem("customCars", JSON.stringify([newCar, ...cars]))
-  }
+    localStorage.setItem('customCars', JSON.stringify([newCar, ...cars]));
+  };
 
   const debouncedSearch = debounce((arg) => {
-    setSearch((arg))
-  }, 800)
+    setSearch(arg);
+  }, 800);
 
-  const handleSearch = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     debouncedSearch(e.target.value.trim());
   };
 
-  const visibleCars = cars.filter(row => {
+  const visibleCars = cars.filter((row) => {
     const columnsToSearchIn = [
       row.car,
       row.car_color,
@@ -166,14 +170,14 @@ const App: React.FC = () => {
       row.car_vin.toString(),
       row.price.toString(),
       row.availability ? 'Yes' : 'No',
-    ]
-    
-    return columnsToSearchIn.some(columnToSearch => 
-      columnToSearch === 'Yes' || columnToSearch === 'No' 
-        ? columnToSearch.toLowerCase() === search.toLowerCase() 
+    ];
+
+    return columnsToSearchIn.some((columnToSearch) =>
+      columnToSearch === 'Yes' || columnToSearch === 'No'
+        ? columnToSearch.toLowerCase() === search.toLowerCase()
         : columnToSearch.toLowerCase().includes(search.toLowerCase())
-    )
-  })
+    );
+  });
 
   return (
     <div className='cars-body'>
@@ -181,34 +185,29 @@ const App: React.FC = () => {
         <Header />
 
         <div className='cars-list-option'>
-          <SearchItems 
-            handleSearch={handleSearch}
-          />
-          <Button 
-            onAddCar={onAddCar}
-          />
+          <SearchItems handleSearch={handleSearch} />
+          <Button onAddCar={onAddCar} />
         </div>
 
         {!cars.length && <IsLoader />}
 
-        <TableCars 
-          cars={visibleCars}
-          columns={columns}
-        />
-      
-        {isDeleteModalOpen 
-          && selectedCar 
-          && <ModalDelete
-              car={selectedCar}
-              onCloseModal={onCloseModal}
-              onRemoveCar={removeCar}
-            />}
-        {isCarModalOpen 
-          && <TestModal 
+        <TableCars cars={visibleCars} columns={columns} />
+
+        {isDeleteModalOpen && selectedCar && (
+          <ModalDelete
+            car={selectedCar}
+            onCloseModal={onCloseModal}
+            onRemoveCar={removeCar}
+          />
+        )}
+        {isCarModalOpen && (
+          <Modal
+            isAddCar={isAddCar}
             car={selectedCar}
             onSubmit={onSaveNewCar}
             onCloseModal={onCloseModal}
-          />}
+          />
+        )}
 
         <ErrorLabel error={error} />
       </div>
